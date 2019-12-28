c253e26144b19d336db624b1e95570caf56715fb
YourBoyE <egcrossmover@gmail.com>
2019-12-27 20:34:33 -0800

    Initial Commit
    1st commit

--- BumpyBalloon/GameScene.swift
+++ BumpyBalloon/GameScene.swift
@@ -9,102 +9,119 @@
 import SpriteKit
 import GameplayKit
 
+@objcMembers
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    let player = SKSpriteNode(imageNamed: "balloon")
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var timer: Timer?
     
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+    override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        //player details
+        player.setScale(0.5)
+        player.zPosition = 1
+        player.position = CGPoint(x: -300, y: 130)
+        addChild(player)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.height / 2.5)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        //adding gravity
+        physicsWorld.gravity = CGVector(dx: 0, dy: -3)
+        
+        parallaxScroll(image: "sky", y: 0, z: -3, duration: 10, needsPhyics: false)
+        parallaxScroll(image: "ground", y: -165, z: -1, duration: 6, needsPhyics: true)
+        
+        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(createObstacle), userInfo: nil, repeats: true)
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        player.physicsBody?.velocity = CGVector(dx: 0, dy: 300)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
+        // to give "pulling up" look
+        let value = player.physicsBody!.velocity.dy * 0.001
+        let rotate = SKAction.rotate(toAngle: value, duration: 0.1)
+        player.run(rotate)
     }
+    
+    func parallaxScroll(image: String, y: CGFloat, z: CGFloat, duration: Double, needsPhyics: Bool) {
+        
+        // run this code twice
+        for i in 0 ... 1 {
+            let node = SKSpriteNode(imageNamed: image)
+            
+            // position the first node on the left, and position the second on the right
+            node.position = CGPoint(x: 1023 * CGFloat(i), y: y)
+            node.zPosition = z
+            addChild(node)
+            
+            //make this node move the width of the screen by whatever duration was passed in
+            let move = SKAction.moveBy(x: -1024, y: 0, duration: duration)
+            
+            // make it jump back to the right edge
+            let wrap = SKAction.moveBy(x: 1024, y: 0, duration: 0)
+            
+            //make these two as a sequence that loops forever
+            let sequence = SKAction.sequence([move, wrap])
+            let forever = SKAction.repeatForever(sequence)
+            
+            //run the animations
+            node.run(forever)
+        }
+        
+    }
+    
+    
+    func createPillar() {
+        
+        let topPillar = SKSpriteNode(imageNamed: "enemy-ground-hi")
+        topPillar.zPosition = -2
+        topPillar.position.x = 400
+        addChild(topPillar)
+        
+        let bottomPillar = SKSpriteNode(imageNamed: "enemy-ground-lo")
+        bottomPillar.zPosition = -2
+        bottomPillar.position.x = topPillar.position.x
+        addChild(bottomPillar)
+    
+        //decide where to create it
+        let rand = GKRandomDistribution(lowestValue: -110, highestValue: 110)
+        topPillar.position.y = CGFloat(rand.nextInt())
+        bottomPillar.position.y = topPillar.position.y - 5
+        
+        //make it move across the screen
+        let action = SKAction.moveTo(x: -700, duration: 8)
+        topPillar.run(action)
+        bottomPillar.run(action)
+    }
+    
+    
+    func createObstacle() {
+        
+        //create and postion the ground, choosing either high or low to make the player dodge them
+        let options = ["enemy-ground-lo", "enemy-ground-hi"]
+        let chosen = GKRandomSource.sharedRandom().nextInt(upperBound: 2)
+        let obstacle = SKSpriteNode(imageNamed: options[chosen])
+        obstacle.zPosition = -2
+        obstacle.position.x = 400
+        addChild(obstacle)
+        
+        //decide where to create it
+        let rand = GKRandomDistribution(lowestValue: -90, highestValue: 100)
+        obstacle.position.y = CGFloat(rand.nextInt())
+        
+        //make it move across the screen
+        let action = SKAction.moveTo(x: -700, duration: 9)
+        obstacle.run(action)
+    }
+    
 }
+
